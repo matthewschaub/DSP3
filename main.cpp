99ba8273e2d6dd9040b3be1b35429d3b00606c41
MattSchaub <matthew.e.schaub@gmail.com>
2018-04-08 19:57:17 -0400

    compiles with class defined and prints result table nullified

--- main.cpp
+++ main.cpp
@@ -3,31 +3,72 @@
 #include <time.h>
 #include <iomanip>
 
-void printResults(); 
+class Hash{
+private:
+  const int tableSize = 10001; 
+public:
+  int* inputStream;
+  int getTablesize();
+  int linearHash(int*, int, int);  
+  void printResults();
+};
+
 int main(){
-  const int tableSize = 10001;
+
+  Hash hashObj; 
+  //const int tableSize = 10001;
   srand(time(NULL));
   
   //Create and initialize a random sequence to be used as the input stream
   //Note, these are not independent sequeneces but build on the previous
   //i.e the stream for 1000 is random, stream for 2000 is the first 1000 + 1000 new random
-  int inputStream[tableSize - 1];
-  for(int i = 0; i < tableSize - 1; i++)
+  int inputStream[hashObj.getTablesize() - 1];
+  for(int i = 0; i < hashObj.getTablesize() - 1; i++)
   {
     inputStream[i] = rand(); 
   }
+  hashObj.inputStream = inputStream; 
   //Creates an array to be mapped to
+  //int hashTable[tableSize]; 
+  //for(int i = 0; i < tableSize; ++i)
+  //{
+  // hashTable[i] = -1;
+  //}
+
+
+  hashObj.printResults();
+
+  return 0; 
+}
+
+int Hash::getTablesize(){
+    return tableSize;
+}
+
+int Hash::linearHash(int* input, int streamSize, int tableSize){
+  int collisions = 0; 
   int hashTable[tableSize]; 
   for(int i = 0; i < tableSize; ++i)
   {
     hashTable[i] = -1;
   }
-  printResults();
+  //
+  for(int i = 0; i < streamSize; i++)
+  {
+    int j = input[i]%streamSize; 
+    if(hashTable[j] != -1)
+    {
+      collisions++;
+      j++;  
+    }
+    else
+      hashTable[j] = input[i];
 
-  return 0; 
+  }
+  return collisions; 
 }
 
-void printResults() //prints the menu
+void Hash::printResults() //prints the menu
 {
   std::cout << std::setw(91) << std::setfill('=') << '\n'<< std::setfill(' ');
   std::cout << std::left << std::setw(42) << ' ' <<  "Results" << std::setw(18) << std::right << "\n";
@@ -37,7 +78,6 @@ void printResults() //prints the menu
   {
     std::cout << std::right << std::setw(5) << i*1000 << std::setw(2) << std::right << '|'; 
   }    
-  //std::right << std::setw(5) << "1000" << std::setw(2) << std::right << '|'<< "2000" << "3000" << "4000" << "5000" << "6000" << "7000" <<"8000" << "9000" << "10000" << std::setw(14) << std::right << "|\n";
   std::cout << "\nLinear" << std::setw(14) << std::right << '|';
   for(int i = 1; i < 11; i++)
   {
