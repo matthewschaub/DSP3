153dab4722341400694d797183181f0af38b6d79
MattSchaub <matthew.e.schaub@gmail.com>
2018-04-08 23:51:08 -0400

    all working

--- main.cpp
+++ main.cpp
@@ -9,33 +9,26 @@ private:
 public:
   int* inputStream;
   int getTablesize();
-  int linearHash(int*, int, int);  
+  int linearHash(int*, int, int);
+  int quadraticHash(int*, int, int);
+  int quadraticHash(int*, int, int, char);
+  int doubleHash(int*, int, int);  
+  int doubleHash(int*, int, int, char); 
   void printResults();
+
 };
 
 int main(){
 
   Hash hashObj; 
-  //const int tableSize = 10001;
+
   srand(time(NULL));
-  
-  //Create and initialize a random sequence to be used as the input stream
-  //Note, these are not independent sequeneces but build on the previous
-  //i.e the stream for 1000 is random, stream for 2000 is the first 1000 + 1000 new random
   int inputStream[hashObj.getTablesize() - 1];
   for(int i = 0; i < hashObj.getTablesize() - 1; i++)
   {
     inputStream[i] = rand(); 
   }
   hashObj.inputStream = inputStream; 
-  //Creates an array to be mapped to
-  //int hashTable[tableSize]; 
-  //for(int i = 0; i < tableSize; ++i)
-  //{
-  // hashTable[i] = -1;
-  //}
-
-
   hashObj.printResults();
 
   return 0; 
@@ -44,10 +37,162 @@ int main(){
 int Hash::getTablesize(){
     return tableSize;
 }
+int Hash::doubleHash(int* input, int streamSize, int tableSize){
+  int collisions = 0;
+  int failures = 0; 
+  bool inserted; 
+  int hashTable[tableSize]; 
+  for(int i = 0; i < tableSize; ++i)
+  {
+    hashTable[i] = -1;
+  }
 
+  for(int i = 0; i < streamSize; i++)
+  {
+    int key = input[i] % tableSize;
+    int r = 9973 - (input[i] % 9973); 
+    inserted = false; 
+    do
+    {
+      if(hashTable[key] == -1)
+      {
+        hashTable[key] = input[i];
+        inserted = true;
+      }
+      else
+      {
+        collisions++;
+        //key = (key + 1)%tableSize;
+        key = (key + r)%tableSize; 
+        if(input[i] % tableSize == key)
+        {
+          failures++;
+          inserted = true;
+        }
+      }
+    }
+    while(!inserted); 
+  }
+  return collisions;   
+}
+int Hash::doubleHash(int* input, int streamSize, int tableSize, char f){
+  int collisions = 0;
+  int failures = 0; 
+  bool inserted; 
+  int hashTable[tableSize]; 
+  for(int i = 0; i < tableSize; ++i)
+  {
+    hashTable[i] = -1;
+  }
+
+  for(int i = 0; i < streamSize; i++)
+  {
+    int key = input[i] % tableSize;
+    int r = 9973 - (input[i] % 9973); 
+    inserted = false; 
+    do
+    {
+      if(hashTable[key] == -1)
+      {
+        hashTable[key] = input[i];
+        inserted = true;
+      }
+      else
+      {
+        collisions++;
+        //key = (key + 1)%tableSize;
+        key = (key + r)%tableSize; 
+        if(input[i] % tableSize == key)
+        {
+          failures++;
+          inserted = true;
+        }
+      }
+    }
+    while(!inserted); 
+  }
+  return failures;   
+}
+int Hash::quadraticHash(int* input, int streamSize, int tableSize){
+  int collisions = 0;
+  int failures = 0; 
+  bool inserted; 
+  int hashTable[tableSize]; 
+  for(int i = 0; i < tableSize; ++i)
+  {
+    hashTable[i] = -1;
+  }
+
+  for(int i = 0; i < streamSize; i++)
+  {
+    int j = 0; 
+    int key = input[i]%tableSize;
+    inserted = false; 
+    do
+    {
+      if(hashTable[key] == -1)
+      {
+        hashTable[key] = input[i];
+        inserted = true; 
+      }
+      else
+      {
+        collisions++;
+        j++;
+        key = (key + (j*j))%tableSize;
+        if(input[i] % tableSize == key)
+        {
+          failures++;
+          inserted = true;  
+        }
+      }
+    }
+    while(!inserted);
+  }
+  return collisions;   
+}
+int Hash::quadraticHash(int* input, int streamSize, int tableSize, char f){
+  int collisions = 0;
+  int failures = 0; 
+  bool inserted; 
+  int hashTable[tableSize]; 
+  for(int i = 0; i < tableSize; ++i)
+  {
+    hashTable[i] = -1;
+  }
+
+  for(int i = 0; i < streamSize; i++)
+  {
+    int j = 0; 
+    int key = input[i]%tableSize;
+    inserted = false; 
+    do
+    {
+      if(hashTable[key] == -1)
+      {
+        hashTable[key] = input[i];
+        inserted = true; 
+      }
+      else
+      {
+        collisions++;
+        j++;
+        key = (key + (j*j))%tableSize;
+        if(input[i] % tableSize == key)
+        {
+          failures++;
+          inserted = true;  
+        }
+      }
+    }
+    while(!inserted);
+  }
+  return failures;   
+}
 int Hash::linearHash(int* input, int streamSize, int tableSize){
   int collisions = 0; 
   int hashTable[tableSize]; 
+  bool inserted; 
   for(int i = 0; i < tableSize; ++i)
   {
     hashTable[i] = -1;
@@ -55,15 +200,22 @@ int Hash::linearHash(int* input, int streamSize, int tableSize){
   //
   for(int i = 0; i < streamSize; i++)
   {
-    int j = input[i]%streamSize; 
-    if(hashTable[j] != -1)
-    {
-      collisions++;
-      j++;  
+    int j = input[i]%tableSize;
+    inserted = false;  
+    do
+    { 
+      if(hashTable[j] != -1)
+      {
+        collisions++;
+        j = (j+1)%tableSize;  
+      }
+      else
+      {
+        hashTable[j] = input[i];
+        inserted = true; 
+      }
     }
-    else
-      hashTable[j] = input[i];
-
+    while(!inserted);
   }
   return collisions; 
 }
@@ -86,17 +238,22 @@ void Hash::printResults() //prints the menu
   std::cout << "\nQuadratic" << std::setw(11) << std::right << '|';
   for(int i = 1; i < 11; i++)
   {
-    std::cout << std::right << std::setw(5) << "xxxx" << std::setw(2) << std::right << '|'; 
-  }    
+    std::cout << std::right << std::setw(5) << quadraticHash(inputStream, i*1000, getTablesize()) << std::setw(2) << std::right << '|'; 
+  }
+  std::cout << "\nDouble" << std::setw(14) << std::right << '|';
+  for(int i = 1; i < 11; i++)
+  {
+    std::cout << std::right << std::setw(5) << doubleHash(inputStream, i*1000, getTablesize()) << std::setw(2) << std::right << '|'; 
+  }  
   std::cout << "\nFailures Quadratic" << std::setw(2) << std::right << '|';
   for(int i = 1; i < 11; i++)
   {
-    std::cout << std::right << std::setw(5) << "xxxx" << std::setw(2) << std::right << '|'; 
+    std::cout << std::right << std::setw(5) << quadraticHash(inputStream, i*1000, getTablesize(), 'f') << std::setw(2) << std::right << '|'; 
   }  
   std::cout << "\nFailures Double" << std::setw(5) << std::right << '|';
   for(int i = 1; i < 11; i++)
   {
-    std::cout << std::right << std::setw(5) << "xxxx" << std::setw(2) << std::right << '|'; 
+    std::cout << std::right << std::setw(5) << doubleHash(inputStream, i*1000, getTablesize(), 'f') << std::setw(2) << std::right << '|'; 
   }  
   std::cout << std::endl << std::setw(91) << std::setfill('=') << '\n'<< std::setfill(' ');
 } 
